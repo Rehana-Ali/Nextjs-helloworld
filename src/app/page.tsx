2fb7ac1cc21887c49125d7aeae6c9551a7fc9c51
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-09-17 12:34:18 -0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Homepage(){
+  return(
+    <div>
+    <h1><b><u>Hello world</u></b></h1>
+    <h2><i>Alhamdolliah I have started my Next.js journey with a simple yet significant "Hello world" project.</i></h2>
+    </div>
+  )
+}
